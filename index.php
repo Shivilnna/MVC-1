eb2ad2fd03501a8d1aed4bdf3217781b63f50f78
Shivilnna <pep.poupardin@gmail.com>
2017-01-09 09:54:52 +0100

    Ajout index.php vierge.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by P.E.
+ * Date: 09/01/2017
+ * Time: 09:48
+ */
+class Model {
+    public $string;
+
+    public function __construct()
+    {
+        $this -> string = "Le MVC, c'est le mal dès le matin !";
+    }
+}
